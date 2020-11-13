f31b3f3cd2421c73749b2a2b258381394ed63ad7
Kyeongjin Kim <krkgj0526@naver.com>
2020-11-13 09:41:43 +0900

    v0.2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+function Food({favorite}) {
+  console.log(favorite);
+  return <h3>I LOVE {favorite}</h3>;
+}
+// react masic : props 안에 <>에서 정의한 prop들이 저장되어있다.
+// props.favorite == { favorite }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1>Hellow!</h1>
+    <Food favorite="kimchi"/>
+    <Food favorite="ramen"/>
+    <Food favorite="samgyeopsal"/>
+    <Food favorite="chukumi"/>
     </div>
   );
 }
+// Food 컴포넌트에 kimchi라는 value로 prop name을 주었고, prop들을 <> 안에서 만들어낼 수 있다.
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App/>, document.getElementById("potato")); 
+// App.js를 가져와서, index.html의 id 중 root에 해당하는 공간을 찾아 render 해준다.
+// <App/> 이것을 컴포넌트라고 한다. React는 컴포넌트로 동작
+// 컴포넌트는 HTML을 반환하는 함수이다. -> JSX의 개념은 React에서 유일함.
+// React application은 한 번에 하나의 component만 rendering 할 수 있다. 기본적으로 App.js에 컴포넌트를 넣는다.
