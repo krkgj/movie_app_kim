7b143c4f6a799c52458ecf77718a0bcea19e56fa
Kyeongjin Kim <krkgj0526@naver.com>
2020-11-16 00:41:50 +0900

    v0.5

--- src/component/Navigation.js
+++ src/component/Navigation.js
@@ -5,13 +5,12 @@ import "./Navigation.css";
 function Navigation() {
   return (
     <div className="navigation_body">
-      <Link className="nav_link1" to="/" style={{ textDecoration: "none" }}>
+      <Link className="nav_link1" to="/">
         HOME
       </Link>
       <Link
         className="nav_link2"
         to={{ pathname: "/about", state: { fromNavigation: true } }}
-        style={{ textDecoration: "none" }}
       >
         ABOUT
       </Link>
